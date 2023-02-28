c2bd0cdcae8b38301fa8440cc5dc3440c1675c70
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-02-28 23:30:30 +0530

    Navbar component completed

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './navbar.css'
+function Nav() {
+    return (
+        <div className="nav">
+        <div className="nav-smily">
+            <img src="./images/memes.png" alt="smily" />
+            <h1>Meme Generataor</h1>
+        </div>
+            <h5>React Course - Project 3</h5>
+        </div>
+    )
+}
+
+export default Nav;
